1288001f61b6e6d1f21cf885c41f20d91f000d05
girgi2mx <47456623+girgi2mx@users.noreply.github.com>
2019-06-14 10:17:38 -0400

    Create createCompany.sql

---
+++ createCompany.sql
@@ -0,0 +1,9 @@
+CREATE TABLE companies(
+	symbol varchar(40), 
+	name varchar (80), 
+	market_cap float, 
+	ipo_year int, 
+	sector varchar(80), 
+	industry varchar(80), 
+	summary_quote varchar(100)
+); 
